e289a3f300bb745853f68248bf4bb8e80c854f61
小樽端倶(AxolotlProject) <otarutrpg@gmail.com>
2026-03-04 18:05:43 +0900

    Create script_index.js

---
+++ script_index.js
@@ -0,0 +1,7 @@
+function blog(x){
+    if(x == 0){
+        alert("選択されたサイトは、現在調整中です");
+    }else if(x == 1){
+        window.location.href = "https://otaruharg.github.io/blog_01/index.html";
+    }
+}
